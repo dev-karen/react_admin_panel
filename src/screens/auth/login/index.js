e22ede8b6652ee397d2ecb72049f806fed275a7e
parisa <you@example.com>
2021-11-24 08:02:47 -0800

    login screen is done

--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -1,4 +1,34 @@
+import { Form, Input, Button } from "antd";
+import Style from "./style";
+
+const { Item } = Form;
 export function Login() {
-  return <p>THIS IS LOGIN</p>;
+  const [form] = Form.useForm();
+  function handleSubmit(values) {
+    console.log(values);
+  }
+  return (
+    <Style>
+      <Form form={form} onFinish={handleSubmit}>
+        <Item
+          name="email"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input placeholder="please enter your email" />
+        </Item>
+        <Item name="password">
+          <Input.Password
+            visibilityToggle={true}
+            placeholder="please your password"
+          />
+        </Item>
+        <Item>
+          <Button type="primary" block htmlType="submit">
+            login
+          </Button>
+        </Item>
+      </Form>
+    </Style>
+  );
 }
 export default Login;
---
+++ src/screens/auth/login/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+export const Style = styled.section`
+  max-width: 500px;
+  margin: 50px auto;
+`;
+export default Style;
